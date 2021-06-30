2a887b74d47a12637569cad23339dcf83aa4d514
RICARDO DA COSTA PASCHOALI <47854361+HarryRicardo1989@users.noreply.github.com>
2021-06-29 23:44:53 -0300

    Aula 10

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import GlobalStyle from "./styles/GlobalStyle";
-import Dashboard from "./pages/Dashboard";
+import Layout from "./components/Layout";
 
 
 const App: React.FC = () => {
     return (
         <>
         <GlobalStyle />
-        <Dashboard />
+        <Layout />
         </>
     );
 }
--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -8,5 +8,18 @@ export default createGlobalStyle`
     box-sizing: border-box;
 }
 
+html , body, #root{
+     height: 100%;
+ }
+
+*,button , input {
+     border:0;
+     outline: 0;
+     font-family: 'Roboto', sans-serif;
+ }
+
+button {
+    cursor: pointer;
+}
 
 `;
