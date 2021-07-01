710737640a65ad51b148ae3072db9998878ccb4f
RICARDO DA COSTA PASCHOALI <47854361+HarryRicardo1989@users.noreply.github.com>
2021-06-30 22:20:00 -0300

    Aula 14

---
+++ src/components/Aside/styles.ts
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+grid-area: AS;
+
+background-color: ${props => props.theme.colors.secondary};
+padding-left: 20px;
+border-right: 1px solid ${props => props.theme.colors.gray};
+
+
+`
+export const Header = styled.header `
+    
+    display: flex;
+    align-items: center;
+
+
+`;
+export const LogImg = styled.img `
+    height: 40px;
+    width: 40px;
+
+
+`;
+export const Title = styled.h3`
+    color: ${props => props.theme.colors.white};
+    margin-left: 10px;
+
+`;
+export const MenuContainer = styled.nav `
+
+
+
+`;
+export const MenuItemLink = styled.a`
+
+
+
+`;
