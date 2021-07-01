a493d396f52af18526a9d07abde2601339059dad
RICARDO DA COSTA PASCHOALI <47854361+HarryRicardo1989@users.noreply.github.com>
2021-07-01 20:37:54 -0300

    Aula 15

--- src/app.tsx
+++ src/app.tsx
@@ -3,15 +3,17 @@ import {ThemeProvider} from 'styled-components'
 import GlobalStyle from "./styles/GlobalStyle";
 import Layout from "./components/Layout";
 import dark from "./styles/themes/dark";
+import Dashboard from "./pages/Dashboard";
 //import light from "./styles/themes/light";
-
 
 
 const App: React.FC = () => {
     return (
         <ThemeProvider theme={dark}>
             <GlobalStyle />
-            <Layout />
+            <Layout>
+                <Dashboard/>
+            </Layout>
         </ThemeProvider>
     );
 }
